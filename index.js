56d41d878ab110056b7217ee4f7eb71ddf4cbb9a
amankumar1020 <amankumarchouhan1020@gmail.com>
2024-06-22 21:51:32 +0530

    cors header

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
-app.use(cors());
+//app.use(cors());
+app.use((req, res, next) =>{
+	  res.setHeader('Access-Control-Allow-Origin','https://musicfrontend-zeta.vercel.app');
+	  res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT,PATCH, DELETE');
+	  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	  next();
+	});
 
 connectDB();
 
